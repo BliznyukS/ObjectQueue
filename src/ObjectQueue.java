2bee1bdea148f6bab1309ab8e402e1bc8ca005a7
BliznyukS <bliznyuk.sergey@gmail.com>
2016-03-26 19:51:30 +0200

    Queue

---
+++ src/ObjectQueue.java
@@ -0,0 +1,14 @@
+public class ObjectQueue
+{
+    public void push(Object obj) {
+        // Пока реализации нет
+    }
+
+    public Object pull() {
+        // Пока реализации нет
+    }
+
+    public int size() {
+        // Пока реализации нет
+    }
+}
